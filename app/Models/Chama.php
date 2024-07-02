6a713252932bb69ba97a826c9dfe13982bcddb77
victor kimanga <vkimanga@gmail.com>
2024-07-02 14:18:48 +0300

    dashboard

---
+++ app/Models/Chama.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chama extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'name',
+        'description',
+        'Number_of_members',
+        'Chama_type',
+        'currency',
+    ];
+    public function Contributions():BelongsTo
+    {
+        return $this->belongsTo(Contributions::class);
+    }
+
+
+
+}
---
+++ app/Models/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\HasTenants;
+use Filament\Notifications\Collection;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable = [
+        'name',        // Changed from 'Name' to 'name'
+        'id_number',   // Changed from 'ID_number' to 'id_number'
+        'email',
+        'password',
+        'image_path'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+    //  public function Admins(): BelongsToMany
+    // {
+    //     return $this->belongsToMany(Admin::class);
+    // }
+
+    // public function getTenants(Panel $panel): Collection
+    // {
+    //     return $this->Admins;
+    // }
+
+    // public function canAccessTenant(Model $tenant): bool
+    // {
+    //     return $this->Admins()->whereKey($tenant)->exists();
+    // }
+}
